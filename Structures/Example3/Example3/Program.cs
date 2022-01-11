a3e86ebd839aedfeb3ecf432156e6c8839e04f6f
Juan Manuel Lorenzo López <juanmanuel.lorenzolopez@gmail.com>
2022-01-11 17:32:15 +0100

    Example 3.

---
+++ Structures/Example3/Example3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test(10);
+            Console.WriteLine(test.Number);
+            ChangeNumber(test);
+            Console.WriteLine(test.Number);
+        }
+        public static void ChangeNumber(Test test)
+        {
+            test.Number = 45;
+        }
+    }
+}
---
+++ Structures/Example3/Example3/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    struct Test
+    {
+        public int Number { get; set; }
+        public Test(int number)
+        {
+            Number = number;
+        }
+    }
+}
