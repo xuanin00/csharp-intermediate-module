fd4432224dceec04a83f97902c8e06f02f2d684b
xuanin00 <juanmanuel.lorenzolopez@gmail.com>
2022-01-11 13:36:31 +0100

    Example 2

---
+++ Structures/Example2/Example2/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    struct Test
+    {
+        public int Number { get; set; }
+        public Test(int number)
+        {
+            Number = number;
+        }
+    }
+}
