a0aea6fb4b6d5454799f8a7d2868f4fdfe217f5d
Juan Manuel Lorenzo López <juanmanuel.lorenzolopez@gmail.com>
2022-01-27 19:49:57 +0100

    no message

--- Delegates/DelegateExample/DelegateExample/Program.cs
+++ Delegates/DelegateExample/DelegateExample/Program.cs
@@ -31,8 +31,14 @@ namespace DelegateExample
             Func<int, int, int> sumDelegate = Sum;
             Console.WriteLine(sumDelegate(10, 20));
 
+            Func<int, int, int> sumDelegateAnonymous = delegate (int op1, int op2) { return op1 + op2; };
+            Console.WriteLine(sumDelegateAnonymous(10, 20));
+
             Action<string> writeDelegate = Write;
             writeDelegate("String parameter to write.");
+
+            Action<string> writeDelegateAnonymous = delegate (string text) { Console.WriteLine(text); };
+            writeDelegateAnonymous("String parameter to write.");
         }
     }
 }
